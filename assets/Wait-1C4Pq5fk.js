5fb3b530b5ed421c86fd07096086a9412b610810
xtco3o <xtco3o@gmail.com>
2026-05-21 12:16:31 +0000

    Deploying to gh-pages from @ webc-zh/webc-zh@e05b6d2cb9c9a9b8d03fe1df975a90f113eac253 🚀

---
+++ assets/Wait-1C4Pq5fk.js
@@ -0,0 +1,26 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./Demo-CC-XRaQa.js","./Index-j-_esVTK.js","./init-DMqhykfX.js","./init-ClQSGNbl.css","./legacy-DQ6vta3t.js"])))=>i.map(i=>d[i]);
+import{t as e}from"./Index-j-_esVTK.js";var t=[`优雅的加载动画与表单提交遮罩
+
+## 功能
+
+- 独立加载：在元素上添加 \`.wait\` 类显示加载图标
+- 表单遮罩：在表单上添加 \`.ing\` 类显示半透明遮罩与加载图标，防止重复提交
+
+## 使用
+
+\`\`\`svelte
+<template lang="pug">
+// 独立加载动画
+b.wait
+
+// 表单提交遮罩
+form.ing
+  input(type="text")
+  button(type="submit") 提交
+</template>
+
+<style lang="stylus">
+@import "com/Wait/Wait.styl"
+</style>
+\`\`\`
+`,()=>e(()=>import(`./Demo-CC-XRaQa.js`),__vite__mapDeps([0,1,2,3,4]),import.meta.url)];export{t as default};
---
+++ assets/legacy-DQ6vta3t.js
@@ -0,0 +1,1 @@
+import{m as e}from"./Index-j-_esVTK.js";e();
